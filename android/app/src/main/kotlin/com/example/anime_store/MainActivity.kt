1472ea34f85a798c64a51c3e88dec043520014fe
baran.ates <baran.ates@accenture.com>
2024-03-15 22:28:56 +0300

    some refactorizations made

--- android/app/src/main/kotlin/com/example/anime_store/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/anime_store/MainActivity.kt
@@ -3,42 +3,41 @@ package com.example.anime_store
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.runBlocking
 import java.io.IOException
 import java.net.HttpURLConnection
 import java.net.URL
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
 
 class MainActivity : FlutterActivity() {
     private val channel = "com.example/anime_store"
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
-        MethodChannel(
-            flutterEngine.dartExecutor.binaryMessenger,
-            channel
-        ).setMethodCallHandler { call, result ->
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, channel).setMethodCallHandler {
+                call,
+                result ->
             val url = call.argument<String>("url")
             runBlocking(Dispatchers.IO) {
-                val response = when (call.method) {
-                    "fetchAnimeList" -> fetchAnimeList(url)
-                    "fetchAnimeCharacters" -> fetchAnimeCharacters(url)
-                    else -> null
-                }
-                response?.let {
-                    result.success(it)
-                } ?: run {
-                    result.error(
-                        "1",
-                        "Response returned null.",
-                        "Either the method is not defined or the response is null.",
-                    )
-                }
+                val response =
+                        when (call.method) {
+                            "fetchAnimeList" -> sendRequest(url)
+                            "fetchAnimeCharacters" -> sendRequest(url)
+                            else -> null
+                        }
+                response?.let { result.success(it) }
+                        ?: run {
+                            result.error(
+                                    "1",
+                                    "Response returned null.",
+                                    "Either the method is not defined or the response is null.",
+                            )
+                        }
             }
         }
     }
 
-    private fun fetchAnimeList(
-        url: String?,
+    private fun sendRequest(
+            url: String?,
     ): String? {
         val url = URL(url)
         return try {
@@ -55,22 +54,4 @@ class MainActivity : FlutterActivity() {
             null
         }
     }
-
-    private fun fetchAnimeCharacters(url: String?): String? {
-        val url = URL(url)
-        return try {
-            val connection = url.openConnection() as HttpURLConnection
-            connection.requestMethod = "GET"
-            connection.connect()
-            if (connection.responseCode == HttpURLConnection.HTTP_OK) {
-                connection.inputStream.bufferedReader().use { it.readText() }
-            } else {
-                null
-            }
-        } catch (e: IOException) {
-            e.printStackTrace()
-            null
-        }
-    }
-
 }
